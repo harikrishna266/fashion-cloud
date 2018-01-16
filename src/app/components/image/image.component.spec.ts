f5d44a6ca13932befac060e7256d509799dfcb29
Harikrishna266 <harikrishna266@gmail.com>
2018-01-16 20:57:55 +0530

    improved search

--- src/app/components/image/image.component.spec.ts
+++ src/app/components/image/image.component.spec.ts
@@ -25,9 +25,17 @@ describe('ImageComponent', () => {
   it('should create', () => {
     fixture = TestBed.createComponent(ImageComponent);
     component = fixture.componentInstance;
-    component.details = {tag:'1',uploaded_date:'12',photo_date:'12',ownerName:'11'};
+  
     fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+  it('should create',async(() => {
+    component.details = {tag:'1',imageUrl:'test',uploaded_date:'12',photo_date:'12',ownerName:'11'};
+    let de = fixture.debugElement.query(By.css('.image_list'))
+    fixture.detectChanges();
+    let ele:HTMLBRElement = de.nativeElement;  
+    expect(ele.getAttribute('src')).toBe('test');
+    
+  }))
    
 });
